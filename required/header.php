e92813cb8677f32281b37a46680e084a24288889
Daxanius <38554396+Daxanius@users.noreply.github.com>
2020-02-20 11:21:25 +0100

    Create header.php

---
+++ required/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!-- Minimaal vereiste HMTL code -->
+<html>
+
+ 	<head>
+		<meta charset="utf-8">
+		<title>The Start</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+        	<link href="https://fonts.googleapis.com/css?family=Sriracha&display=swap" rel="stylesheet">
+	</head>
+	<body>
