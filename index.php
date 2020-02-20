3e19bd30782b039d06526b12799629270811c1c6
Daxanius <38554396+Daxanius@users.noreply.github.com>
2020-02-20 20:47:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
         <h1> Welcome to "GTA Online: The Criminal Adventure". This is a story set in the world of GTA Online. You're Choices can change the whole story and the end to it. So Choose wisely !!!
         Click on the Start button te begin.</h1>
-        <a href="php/The_beginning.php"><img src="images/start.jpeg"></a>
+        <a href="php/The_beginning.php" onclick="<?php $_SESSION['prev'] = 'index'; ?>"><img src="images/start.jpeg"></a>
 
 <?php
 	require 'required/footer.php';
