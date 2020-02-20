b5057e637b7fbb713ee0c3715084d516858baba8
Daxanius <38554396+Daxanius@users.noreply.github.com>
2020-02-20 11:23:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<!-- Minimaal vereiste HMTL code -->
-<html>
+<?php
+	require 'required/header.php';
+?>
 
- 	<head>
-		<meta charset="utf-8">
-		<title>The Start</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-        	<link href="https://fonts.googleapis.com/css?family=Sriracha&display=swap" rel="stylesheet">
-	</head>
-
-	<body>
         <h1> Welcome to "GTA Online: The Criminal Adventure". This is a story set in the world of GTA Online. You're Choices can change the whole story and the end to it. So Choose wisely !!!
         Click on the Start button te begin.</h1>
         <a href="html/The_beginning.html"><img src="images/start.jpeg"></a>
-        
-        
-        
 
-
-	</body>
-
-</html>
+<?php
+	require 'required/footer.php';
+?>
