aa2bc56b31b78ac1833b8d2c052dcda0427ebb84
Daxanius <38554396+Daxanius@users.noreply.github.com>
2020-02-20 11:58:58 +0100

    Update and rename boat.html to boat.php

---
+++ html/boat.php
@@ -0,0 +1,27 @@
+<?php
+	require '../required/header.php';
+?>
+
+	<style>
+		body{
+			background-image: url(../images/Gta-background.jpg);
+			background-repeat: no-repeat;
+  			background-attachment: fixed;
+		}
+	</style>
+
+        <h1>You've decided to go by boat </h1>
+        <video width= 30%  src="../videos/Boat.mp4" autoplay controls >
+        </video>
+        
+       
+        <ul>
+        
+        <li> Go to the sea  <a  href="sea.html"><img  style= "width: 450px; padding-left: 10px" src=../images/PacificOcean-GTAV.jpg></a></li>
+        <li>Get on the land<a  href="land.html"><img style= "width: 450px;" src=../images/LandActDam-GTAV-East.png></a></li>
+        
+    </ul>
+        
+<?php
+	require '../required/footer.php';
+?>
