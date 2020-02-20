7c8c9855b8f095167185b9b9538680e0b380b82d
Daxanius <38554396+Daxanius@users.noreply.github.com>
2020-02-20 11:25:56 +0100

    Update header.php

--- required/header.php
+++ required/header.php
@@ -5,7 +5,7 @@
  	<head>
 		<meta charset="utf-8">
 		<title>The Start</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
         	<link href="https://fonts.googleapis.com/css?family=Sriracha&display=swap" rel="stylesheet">
 	</head>
 	<body>
